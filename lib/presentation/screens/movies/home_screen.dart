64704d08e80dbed80c108025766d765b069c6060
David M <elarreglador@protonmail.com>
2024-05-11 20:47:50 +0200

    first commit

---
+++ lib/presentation/screens/movies/home_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+
+  static const name = "home-screen";
+  const HomeScreen({
+    super.key
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    // ignore: prefer_const_constructors
+    return Scaffold(
+      body:  const Placeholder(),
+    );
+  }
+}
