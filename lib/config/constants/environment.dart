39edd7dc295a93a239aab7464d16987b8dc3eab3
David M <elarreglador@protonmail.com>
2024-05-13 15:23:00 +0200

    Configuracion de variables de entorno funcional gracias al paquete dotEnv

--- lib/config/constants/environment.dart
+++ lib/config/constants/environment.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class Environment {
-
+  // OBTIENE LA VARIABLE DE ENTORNO DESDE /.ENV GRACIAS AL PAQUETE DOTENV
   static String theMovieDbKey = dotenv.env["THE_MOVIEDB_KEY"] ?? "THE_MOVIEDB_KEY not found.";
 }
---
+++ lib/infrastructure/moviedb_datasource.dart
@@ -0,0 +1,28 @@
+// este datasource se crea con la intencion de trabajar exclusivamente con 
+// mobiedb, si necesitamos obtener de otras fuentes crearemos un datasource
+// diferente y especializado.
+
+import 'package:cinemapedia/domain/datasources/movies_datasource.dart';
+import 'package:dio/dio.dart';
+import 'package:cinemapedia/domain/entities/movie.dart';
+
+
+
+class MoviedbDatasource extends MovieDatasource {
+
+  final dio = Dio(BaseOptions(
+    baseUrl:  "https://api.themoviedb.org/3",
+    
+  ));
+
+  @override
+  Future<List<Movie>> getNowPlaying({int page = 1}) async {
+    
+
+
+    return [];
+  }
+
+
+
+}
