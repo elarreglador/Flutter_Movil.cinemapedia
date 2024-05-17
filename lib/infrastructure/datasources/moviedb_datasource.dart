25dc3a04c77252631b42caf648212b0d4606bd1e
David M <elarreglador@protonmail.com>
2024-05-17 07:35:35 +0200

    mapeo de la respueta de MovieDB

--- lib/infrastructure/datasources/moviedb_datasource.dart
+++ lib/infrastructure/datasources/moviedb_datasource.dart
@@ -2,9 +2,13 @@
 // mobiedb, si necesitamos obtener de otras fuentes crearemos un datasource
 // diferente y especializado.
 
+import 'package:cinemapedia/config/constants/environment.dart';
 import 'package:cinemapedia/domain/datasources/movies_datasource.dart';
+import 'package:cinemapedia/infrastructure/mappers/movie_mapper.dart';
+import 'package:cinemapedia/infrastructure/models/moviedb/moviedb_response.dart';
 import 'package:dio/dio.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:flutter/material.dart';
 
 
 
@@ -12,16 +16,25 @@ class MoviedbDatasource extends MoviesDatasource {
 
   final dio = Dio(BaseOptions(
     baseUrl:  "https://api.themoviedb.org/3",
-    
+    queryParameters: {
+      'api_key': Environment.theMovieDbKey,
+      'language': 'es-ES'
+    }
   ));
 
   @override
   Future<List<Movie>> getNowPlaying({int page = 1}) async {
     
     final response = await dio.get("/movie/now_playing");
-    final List<Movie> movies = [];
+    final movieDBResponse = MovieDbResponse.fromJson(response.data);
 
-    return [];
+    final List<Movie> movies = movieDBResponse.results
+    .where((moviedb) => moviedb.posterPath != 'no-poster') //FILTRO: Descarta peliculas sin poster
+    .map(
+      (moviedb) => MovieMapper.movieDBToEntity(moviedb)
+    ).toList();
+
+    return movies;
   }
 
 
--- lib/infrastructure/mappers/movie_mapper.dart
+++ lib/infrastructure/mappers/movie_mapper.dart
@@ -20,7 +20,7 @@ class MovieMapper {
     popularity: moviedb.popularity,
     posterPath: (moviedb.posterPath != '')
     ? 'https://image.tmdb.org/t/p/w500${moviedb.posterPath}'
-    : 'https://ih1.redbubble.net/image.1893341687.8294/fposter,small,wall_texture,product,750x1000.jpg',
+    : 'no-poster',
     releaseDate: moviedb.releaseDate,
     title: moviedb.title,
     video: moviedb.video,
