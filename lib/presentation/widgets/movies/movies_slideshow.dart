9b9e9ab02d5706d7e4f4b1d9ce7801cfabbc2627
David M <elarreglador@protonmail.com>
2024-06-04 07:22:10 +0200

    Carrusel con imagenes, carga suave de imagenes con animate_do

--- lib/presentation/widgets/movies/movies_slideshow.dart
+++ lib/presentation/widgets/movies/movies_slideshow.dart
@@ -1,6 +1,8 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:card_swiper/card_swiper.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class MoviesSlideshow extends StatelessWidget {
   final List<Movie> movies;
@@ -36,6 +38,39 @@ class _Slide extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+
+    final decoration = BoxDecoration(
+      borderRadius: BorderRadius.circular(20),
+      boxShadow: const [
+        BoxShadow(
+          color: Colors.black,
+          blurRadius: 10,
+          offset: Offset(0,5)
+        )
+      ]
+    );
+
+    return Padding(
+      padding: const EdgeInsets.only( bottom: 30 ),
+      child: DecoratedBox(
+        decoration: decoration,
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(20),
+          child: Image.network(
+            movie.backdropPath,
+            fit: BoxFit.cover,
+            loadingBuilder: (context, child, loadingProgress){ // Durante la carga de imagenes...
+              if (loadingProgress != null ){
+                return const DecoratedBox(
+                  decoration: BoxDecoration( color: Colors.black) // ... el fondo es negro
+                );
+              }
+
+              return FadeIn(child: child);
+            },
+          )
+          )
+      ),
+    );
   }
 }
