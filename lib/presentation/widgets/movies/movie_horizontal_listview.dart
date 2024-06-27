c6c9b088e8710e16d3ad8ea55ed829b68b7e0192
David M <elarreglador@protonmail.com>
2024-06-27 20:29:01 +0200

    Funion para generar numeros cortos como 3K o 7M usada en la puntuacion de las peliculas

--- lib/presentation/widgets/movies/movie_horizontal_listview.dart
+++ lib/presentation/widgets/movies/movie_horizontal_listview.dart
@@ -1,21 +1,20 @@
 import 'package:animate_do/animate_do.dart';
+import 'package:cinemapedia/config/helpers/Human_formmats.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:flutter/material.dart';
 
 class MovieHorizontalListview extends StatelessWidget {
-
   final List<Movie> movies;
   final String? title;
   final String? subTitle;
   final VoidCallback? loadNextPage;
 
-  const MovieHorizontalListview({
-    super.key,
-    required this.movies, 
-    this.title, 
-    this.subTitle, 
-    this.loadNextPage
-  });
+  const MovieHorizontalListview(
+      {super.key,
+      required this.movies,
+      this.title,
+      this.subTitle,
+      this.loadNextPage});
 
   @override
   Widget build(BuildContext context) {
@@ -24,18 +23,15 @@ class MovieHorizontalListview extends StatelessWidget {
       child: Column(
         children: [
           if (title != null || subTitle != null)
-            _Title(title: title,subTitle: subTitle),
-
+            _Title(title: title, subTitle: subTitle),
           Expanded(
-            child: ListView.builder(
-              itemCount: movies.length,
-              scrollDirection: Axis.horizontal,
-              physics: const BouncingScrollPhysics(),
-              itemBuilder: (context, index) {
-                return _Slide(movie: movies[index]);
-              }
-            )
-          )
+              child: ListView.builder(
+                  itemCount: movies.length,
+                  scrollDirection: Axis.horizontal,
+                  physics: const BouncingScrollPhysics(),
+                  itemBuilder: (context, index) {
+                    return _Slide(movie: movies[index]);
+                  }))
         ],
       ),
     );
@@ -43,86 +39,80 @@ class MovieHorizontalListview extends StatelessWidget {
 }
 
 class _Slide extends StatelessWidget {
-
   final Movie movie;
 
   const _Slide({required this.movie});
 
   @override
   Widget build(BuildContext context) {
-
     final textStyles = Theme.of(context).textTheme;
 
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-
-        // Imagen
-        SizedBox(
-          width: 130,
-          height: 180,
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(20),
-            child: Image.network(
-              fit: BoxFit.fill,
-              movie.posterPath,
-              width: 150,
-              loadingBuilder: (context, child, loadingProgress){
-
-                if (loadingProgress != null) {
-                  return const Padding(
-                    padding: EdgeInsets.all(1),
-                    child: Center(child: CircularProgressIndicator(strokeWidth: 2)),
-                  );
-                }
-
-                return FadeInUp(child: child);
-              },
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // Imagen
+          SizedBox(
+            width: 130,
+            height: 180,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.network(
+                fit: BoxFit.fill,
+                movie.posterPath,
+                width: 150,
+                loadingBuilder: (context, child, loadingProgress) {
+                  if (loadingProgress != null) {
+                    return const Padding(
+                      padding: EdgeInsets.all(1),
+                      child: Center(
+                          child: CircularProgressIndicator(strokeWidth: 2)),
+                    );
+                  }
+      
+                  return FadeInUp(child: child);
+                },
+              ),
             ),
           ),
-        ),
-
-        // Separacion
-        const SizedBox(height: 5),
-
-        // Titulo
-        SizedBox(
-          width: 150,
-          child: Text(
-            movie.title,
-            maxLines: 2,
-            style: textStyles.titleSmall
+      
+          // Separacion
+          const SizedBox(height: 5),
+      
+          // Titulo
+          SizedBox(
+            width: 130,
+            child: Text(movie.title, maxLines: 2, style: textStyles.titleSmall),
           ),
-        ),
-
-        // Puntuacion
-        Row(
-          children: [
-
-            const Icon(
-              Icons.star_half_outlined, 
-              color: Colors.orange
-            ),
-
-            Text('${movie.voteAverage}', style: textStyles.bodyMedium?.copyWith(color:Colors.orange)),
-
-            const SizedBox(width: 15),
-
-            Text("${movie.popularity}", style: textStyles.bodySmall)
-          ],
-        )
-
-      ],
+      
+          // Puntuacion
+          SizedBox(
+              width: 130,
+              child: Row(
+                children: [
+                  const Icon(Icons.star_half_outlined, color: Colors.orange),
+                  Text('${movie.voteAverage}',
+                      style:
+                          textStyles.bodyMedium?.copyWith(color: Colors.orange)),
+                  const SizedBox(width: 15),
+                  const Spacer(),
+                  Text(HumanFormats.number(movie.popularity),
+                      style: textStyles.bodySmall)
+                ],
+              )
+            )
+        ],
+      ),
     );
   }
 }
 
 class _Title extends StatelessWidget {
-
   final String? title;
   final String? subTitle;
 
-  const _Title({ this.title, this.subTitle });
+  const _Title({this.title, this.subTitle});
 
   @override
   Widget build(BuildContext context) {
@@ -131,22 +121,14 @@ class _Title extends StatelessWidget {
       margin: const EdgeInsets.symmetric(horizontal: 10),
       child: Row(
         children: [
-
           if (title != null)
-            Text(
-              title!, 
-              style:  Theme.of(context).textTheme.titleLarge
-            ),
-
+            Text(title!, style: Theme.of(context).textTheme.titleLarge),
           const Spacer(),
-
           if (subTitle != null)
             TextButton(
-              style: const ButtonStyle(visualDensity: VisualDensity.compact),
-              onPressed: (){}, 
-              child: Text(subTitle!)
-            )
-
+                style: const ButtonStyle(visualDensity: VisualDensity.compact),
+                onPressed: () {},
+                child: Text(subTitle!))
         ],
       ),
     );
--- lib/presentation/widgets/movies/movies_slideshow.dart
+++ lib/presentation/widgets/movies/movies_slideshow.dart
@@ -1,11 +1,10 @@
 import 'package:animate_do/animate_do.dart';
+import 'package:flutter/material.dart';
 import 'package:card_swiper/card_swiper.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
-import 'package:flutter/material.dart';
 
 class MoviesSlideshow extends StatelessWidget {
   final List<Movie> movies;
-
   const MoviesSlideshow({
     super.key, 
     required this.movies
@@ -14,7 +13,7 @@ class MoviesSlideshow extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    final colors = Theme.of(context).colorScheme; 
+    final colors = Theme.of(context).colorScheme;
 
     return SizedBox(
       height: 210,
@@ -26,14 +25,12 @@ class MoviesSlideshow extends StatelessWidget {
         pagination: SwiperPagination(
           margin: const EdgeInsets.only(top: 10),
           builder: DotSwiperPaginationBuilder(
-            activeColor: Colors.red,
-            color: colors.primary 
+            activeColor: colors.primary,
+            color: colors.secondary
           )
         ),
         itemCount: movies.length,
-        itemBuilder: (context, index){
-          return _Slide(movie: movies[index]);
-        }
+        itemBuilder: (context, index) => _Slide(movie: movies[index] ),
       ),
     );
   }
@@ -52,7 +49,7 @@ class _Slide extends StatelessWidget {
       borderRadius: BorderRadius.circular(20),
       boxShadow: const [
         BoxShadow(
-          color: Colors.black,
+          color: Colors.black45,
           blurRadius: 10,
           offset: Offset(0,5)
         )
@@ -65,38 +62,17 @@ class _Slide extends StatelessWidget {
         decoration: decoration,
         child: ClipRRect(
           borderRadius: BorderRadius.circular(20),
-          child: Stack(
-            children: [
-              Image.network( //IMAGEN DEL CARRUSEL
-                movie.backdropPath,
-                fit: BoxFit.cover,
-                loadingBuilder: (context, child, loadingProgress){ // Durante la carga de imagenes...
-                  if (loadingProgress != null ){
-                    return const DecoratedBox(
-                      decoration: BoxDecoration( color: Colors.black) // ... el fondo es negro
-                    );
-                  }
-              
-                  return FadeInRight(child: child);
-                },
-              ),
-              Padding( //TITULO DE LA PELICULA
-                padding: const EdgeInsets.fromLTRB(10,10,0,0),
-                child: Text(
-                  movie.title, 
-                  style: const TextStyle(
-                    color: Colors.white,
-                    shadows: [
-                      Shadow(
-                        offset: Offset(2.0, 2.0),
-                        blurRadius: 3.0,
-                        color: Colors.black,
-                      )
-                    ]
-                  )
-                ),
-              )
-            ]
+          child: Image.network(
+            movie.backdropPath,
+            fit: BoxFit.cover,
+            loadingBuilder: (context, child, loadingProgress) {
+              if ( loadingProgress != null ) {
+                return const DecoratedBox(
+                  decoration: BoxDecoration( color: Colors.black12 )
+                );
+              }
+              return FadeIn(child: child);
+            },
           )
         )
       ),
